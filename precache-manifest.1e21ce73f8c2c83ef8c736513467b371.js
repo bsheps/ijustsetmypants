1033e2368dfebdc73dda67563032e457e4b1b113
Brandon Shepard <brandon.shepard6@icloud.com>
2020-03-02 19:08:38 -0600

    Updates

---
+++ precache-manifest.1e21ce73f8c2c83ef8c736513467b371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592b8b26fd70e6e4d890887ede7fca72",
+    "url": "/ijustsetmypants/index.html"
+  },
+  {
+    "revision": "2465f1536ad9a8c8349d",
+    "url": "/ijustsetmypants/static/css/main.46c0f7c7.chunk.css"
+  },
+  {
+    "revision": "3cb71e6948f2e4853758",
+    "url": "/ijustsetmypants/static/js/2.43eb05a8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ijustsetmypants/static/js/2.43eb05a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2465f1536ad9a8c8349d",
+    "url": "/ijustsetmypants/static/js/main.19ebabc2.chunk.js"
+  },
+  {
+    "revision": "8e493ce1355c2b40af62",
+    "url": "/ijustsetmypants/static/js/runtime-main.32899fdd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28ea31f0f2cd4bcefcafec8738e35f29.js"
+  "/ijustsetmypants/precache-manifest.1e21ce73f8c2c83ef8c736513467b371.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ijustsetmypants/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
